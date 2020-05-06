868ca20d5c0b1b693a719f32c964d2ae01238bb5
TANMAYA <vibhorgupta2008@aol.in>
2020-05-07 03:22:52 +0530

    Fix rucocop issues

--- db/db_initiation.rb
+++ db/db_initiation.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'yaml'
 require 'active_record'
 
 currernt_directory = File.dirname(__FILE__)
 db_config_path = "#{currernt_directory}/database.yaml"
 db_config_file = File.open(db_config_path)
-db_config = YAML::load(db_config_file)
+db_config = YAML.safe_load(db_config_file)
 
-db_config["username"] = ENV["paylater_service_username"]
-db_config["password"] = ENV["paylater_service_password"]
+db_config['username'] = ENV['paylater_service_username']
+db_config['password'] = ENV['paylater_service_password']
 
 ActiveRecord::Base.establish_connection(db_config)
