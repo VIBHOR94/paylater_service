958a92249913726d07dc04a24efdeb56e0abd36c
TANMAYA <vibhorgupta2008@aol.in>
2020-05-08 02:51:20 +0530

    Restructure the service folder to work align with models - Rewrite playlater service to and User service to use Service class and object methods

---
+++ services/user_service.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../models/user'
+
+class UserService
+  def initialize(user)
+    @user = user
+  end
+
+  def create_user
+    if @user.save
+      puts "#{user_name} (#{credit_limit})"
+    else
+      puts 'Following error/s occured while creating the record'
+      puts @user.errors.full_messages
+    end
+  end
+
+  def self.fetch_user_instance(command)
+    command_args = command.split(' ')
+
+    user_name = command_args[2]
+    email = command_args[3]
+    credit_limit = command_args[4]
+
+    User.new(name: user_name, email: email, credit_limit: credit_limit)
+  end
+end
