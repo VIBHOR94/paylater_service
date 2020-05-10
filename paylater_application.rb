f00a5b65e1cebb93a45fe758d1cfa88149259fd8
TANMAYA <vibhorgupta2008@aol.in>
2020-05-11 04:45:50 +0530

    Add migration script and fix minor formatting issues.

--- paylater_application.rb
+++ paylater_application.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require_relative './dependencies.rb'
+require_relative './services/user_service.rb'
+require_relative './services/merchant_service.rb'
+require_relative './services/transaction_service.rb'
 
 class PaylaterApplication
   def self.process(command)
